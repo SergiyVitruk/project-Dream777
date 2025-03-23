32627260500b29c38ede2109dff5eaf7c2a757e4
SergiyVitruk <vitruk.sergiy@gmail.com>
2025-03-23 23:45:09 +0200

    new js + debugging v4

--- src/main.js
+++ src/main.js
@@ -12,3 +12,36 @@
     refs.modal.classList.toggle('is-open');
   }
 })();
+
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const mobileMenu = document.querySelector(".mobile-menu");
+    const menuLinks = document.querySelectorAll(".mobile-menu__link");
+    const menuCloseButton = document.querySelector("[data-menu-close]");
+
+    function closeMenu() {
+        mobileMenu.classList.remove("is-open"); 
+    }
+
+    menuLinks.forEach(link => {
+        link.addEventListener("click", function () {
+            closeMenu();
+        });
+    });
+});
